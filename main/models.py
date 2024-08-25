ba76f6707d312fc48ef408eb758d4ef8cd0aa644
Ddaaan <de2572@naver.com>
2024-08-25 14:05:50 +0900

    save

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Response(models.Model):
+    question_id = models.IntegerField()
+    text_ans = models.TextField(blank=True, null=True)
+    score_ans = models.IntegerField(blank=True, null=False)
+    
+    def __str__(self):
+        return f"질문에 답변하세요 {self.question_id}"
