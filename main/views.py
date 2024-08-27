1754ffa24d7a0c4e7f7ad7f8a1a69e7c04776805
Ddaaan <de2572@naver.com>
2024-08-27 17:53:38 +0900

    로그인/설문조사 폼 시도

--- main/views.py
+++ main/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic import View
+
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 def main_index(request):
@@ -23,3 +27,30 @@ def admin(request):
 
 def student_survey(request):
     return render(request, 'student_survey.html')
+
+def stuSur_question(request):
+    questions = [
+        {'id' : 1, 'text' : '1번 문제 테스트입니다.'},
+        {'id' : 2, 'text' : '2번 문제 테스트입니다.'}
+    ]
+    options = range(1, 6)
+    return render(request, 'student_survey.html', {'questions' : questions, 'options' : options})
+
+# 로그인 화면 
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("/home")
+            else:
+                messages.error(request,"Invalid username or password.")
+        else:
+            messages.error(request,"Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request=request, template_name="main/login.html", context={"login_form":form})
--- surveySite/settings.py
+++ surveySite/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'crispy_forms', #add
+    #'accounts.apps.AccountsConfig', #add
 ]
 
 MIDDLEWARE = [
