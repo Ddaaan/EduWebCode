ac4c0f03faeed199d7133bc70a424b151b4cfd85
Ddaaan <de2572@naver.com>
2024-09-13 19:06:54 +0900

    지역청/본청 아이디 비번 생성, DB저장

--- main/management/commands/update_school_password.py
+++ main/management/commands/update_school_password.py
@@ -14,12 +14,15 @@ class Command(BaseCommand):
         ws = wb.active
 
         for row in ws.iter_rows(min_row=2, values_only=True):
-            school_id = row[7]  # 아이디가 8번째 열에 있다고 가정
+            school_name = row[3]  # 학교이름이 4번째 열에 있다고 가정
+            district = row[4]  # 지역이 5번째 열에 있다고 가정
+            id = row[7]  # 아이디가 8번째 열에 있다고 가정
             password = row[8]   # 비밀번호가 9번째 열에 있다고 가정
             try:
-                school = School.objects.get(school_id=school_id)
+                school = School.objects.get(school_name=school_name, district=district)
+                school.school_id = id
                 school.school_pw = password
                 school.save()
-                self.stdout.write(self.style.SUCCESS(f'{school_id}의 비밀번호가 성공적으로 업데이트되었습니다.'))
+                self.stdout.write(self.style.SUCCESS(f'{school_name}의 비밀번호가 성공적으로 업데이트되었습니다.'))
             except School.DoesNotExist:
-                self.stdout.write(self.style.ERROR(f'{school_id}에 해당하는 학교를 찾을 수 없습니다.'))
+                self.stdout.write(self.style.ERROR(f'{school_name}에 해당하는 학교를 찾을 수 없습니다.'))
